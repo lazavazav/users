d500696d339f470d1cac16139a2a30c9b51e9056
lazavazav <lazav2021@gmail.com>
2022-01-21 13:41:31 -0500

    update titles

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-// fetch('http://localhost:8888/api/readAll')
-//   .then((response) => response.json())
-//   .then((json) => console.log({ json }));
-
 document.getElementById('seeData').addEventListener('click', appendData);
 function appendData() {
   fetch('https://hardcore-mayer-7938e9.netlify.app/.netlify/functions/readAll')
     .then((response) => response.json())
     .then((data) => {
-      var mainContainer = document.getElementById('myData');
-      for (var i = 0; i < data.data.length; i++) {
-        var div = document.createElement('div');
-        div.innerHTML = 'Indicators: ' + data.data[i];
+      let mainContainer = document.getElementById('myData');
+      let titleBox = document.getElementById('title');
+      for (let i = 0; i < data.data.length; i++) {
+        let div = document.createElement('div');
+        titleBox.innerHTML = "Indicators for Women's Health";
+        div.innerHTML = data.data[i];
         mainContainer.appendChild(div);
       }
     })
