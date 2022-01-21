243393548a26afeb2d388c892f3c3c58290e740c
lazavazav <lazav2021@gmail.com>
2022-01-21 13:32:21 -0500

    updated fetch url

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 document.getElementById('seeData').addEventListener('click', appendData);
 function appendData() {
-  fetch('https://hardcore-mayer-7938e9.netlify.app/api/readAll')
+  fetch('https://hardcore-mayer-7938e9.netlify.app/.netlify/functions/readAll')
     .then((response) => response.json())
     .then((data) => {
       var mainContainer = document.getElementById('myData');
