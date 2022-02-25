bf32bc4299c88c19c58a96f9e576b7376b8dbe76
lazavazav <lazav2021@gmail.com>
2022-02-25 12:28:35 -0500

    updated index.html and deleted unused functions

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-document.getElementById('seeData').addEventListener('click', appendData);
-function appendData() {
-  fetch('https://hardcore-mayer-7938e9.netlify.app/.netlify/functions/readAll')
-    .then((response) => response.json())
-    .then((data) => {
-      let mainContainer = document.getElementById('myData');
-      let titleBox = document.getElementById('title');
-      for (let i = 0; i < data.data.length; i++) {
-        let div = document.createElement('div');
-        titleBox.innerHTML = "Indicators for Women's Health";
-        div.innerHTML = data.data[i];
-        mainContainer.appendChild(div);
-        document.getElementById('seeData').disabled = true;
-      }
-    })
-    .catch(console.error);
-}
+// document.getElementById('seeData').addEventListener('click', appendData);
+// function appendData() {
+//   fetch('https://hardcore-mayer-7938e9.netlify.app/.netlify/functions/readAll')
+//     .then((response) => response.json())
+//     .then((data) => {
+//       let mainContainer = document.getElementById('myData');
+//       let titleBox = document.getElementById('title');
+//       for (let i = 0; i < data.data.length; i++) {
+//         let div = document.createElement('div');
+//         titleBox.innerHTML = "Indicators for Women's Health";
+//         div.innerHTML = data.data[i];
+//         mainContainer.appendChild(div);
+//         document.getElementById('seeData').disabled = true;
+//       }
+//     })
+//     .catch(console.error);
+// }
 
 function getOption() {
   selectElement = document.querySelector('#selectOptions');
